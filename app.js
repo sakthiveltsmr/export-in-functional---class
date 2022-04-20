e32e7504425d4de52332964b5388711122fa94ea
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-04-20 13:21:28 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+import { Employee, demo } from "./export.js";
+
+Employee("sakthi");
+
+let p = new demo(24);
+
+p.display();
---
+++ export.js
@@ -0,0 +1,13 @@
+//Named export
+export function Employee(name) {
+  console.log(name);
+}
+
+export class demo {
+  constructor(age) {
+    console.log(age);
+  }
+  display() {
+    console.log(`welcome to sakthi`);
+  }
+}
